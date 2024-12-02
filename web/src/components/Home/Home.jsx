08f17320efd7ffcd980f73b002cd913e18e34489
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-02 08:21:41 +0100

    hero

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Hero from './sections/1-hero/hero'
 
 export default function Home() {
   return (
-    <main style={{ height: '200vh' }}></main>
+    <main>
+      <Hero />
+
+      <div style={{ height: '200vh' }} />
+    </main>
   )
 }
---
+++ web/src/components/Home/sections/1-hero/hero.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import styles from './hero.module.css';
+import leftImg from '../../../../assets/images/Home/left.avif';
+import centerImg from '../../../../assets/images/Home/main.png';
+import rightImg from '../../../../assets/images/Home/right.jpg';
+import leaf from '../../../../assets/images/backgrounds/0.png';
+import leafTwo from '../../../../assets/images/backgrounds/1.png';
+import leafThree from '../../../../assets/images/backgrounds/2.png';
+import { BiCursorFill, HugeiconsFileAdd } from '../../../svgs/svg';
+
+export default function Hero() {
+    return (
+        <section className={styles.section}>
+            <div className={styles.contentWrapper}>
+                <h1>We provide the best recipes for you</h1>
+                <p className={styles.description}>
+                    Share Your Favorite Recipes, Tips. Browse through a vast collection of recipes from all over the world. Whether you're looking for quick weeknight dinners, decadent desserts, or healthy meals, you'll find it all here.
+                </p>
+
+                <div className={styles.buttonContainer}>
+                    <Link to='/recipes' className={`${styles.button} ${styles.secondaryButton}`}>
+                        <BiCursorFill className={styles.buttonIcon} /> Discover
+                    </Link>
+                    <Link to='/recipes' className={`${styles.button} ${styles.primaryButton}`}>
+                        <HugeiconsFileAdd className={styles.buttonIcon} /> Create Recipe
+                    </Link>
+                </div>
+            </div>
+            <div className={styles.imageContainer}>
+                <img src={leaf} className={`${styles.leaf} ${styles.topRight}`} />
+                <img src={leafTwo} className={`${styles.leaf} ${styles.topLeft}`} />
+                <img src={leafThree} className={`${styles.leaf} ${styles.bottomRight}`} />
+
+                <div className={`${styles.sideImageWrapper} ${styles.leftImage}`}>
+                    <img src={leftImg} className={styles.image} />
+                </div>
+                <div className={styles.centerImageWrapper}>
+                    <img src={centerImg} className={styles.image} />
+                </div>
+                <div className={`${styles.sideImageWrapper} ${styles.rightImage}`}>
+                    <img src={rightImg} className={styles.image} />
+                </div>
+            </div>
+        </section>
+    );
+}
